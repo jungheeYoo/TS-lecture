ffbc451d9ff5c166b6eaef37f9d8d04554e2f5f8
Joy <jungheeyoo.joy@gmail.com>
2024-07-02 22:53:29 +0900

    TypeScript Challenge Lecture

--- 4-5.polymorphism.ts
+++ 4-5.polymorphism.ts
@@ -10,12 +10,19 @@
 // Storage는 타입스크립트에 의해 이미 선언된 자바스크립트의 웹 스토리지 API를 위한 인터페이스임
 // 그래서 이름 다르게 함 SStorage
 interface SStorage<T> {
-  // 이것은 key가 제한되지 않은 오브젝트를 정의하게 해줌
+  // : T; => 이것은 key가 제한되지 않은 오브젝트를 정의하게 해줌
   [key: string]: T;
 }
 
+// constructor()를 사용하지 않아도 됨
+// TypeScript에서는 클래스의 필드를 선언할 때 초기값을 직접 지정하면, 해당 값으로 자동으로 초기화된다.
+// private storage: SStorage<T> = {};는
+// storage 필드를 빈 객체로 초기화하는 코드
+// 이 경우, 명시적인 생성자 없이도 필드가 적절하게 초기화 된다
+// storage 객체 이름
 class LocalStorage<T> {
   private storage: SStorage<T> = {};
+
   set(key: string, value: T) {
     //  key와 value를 스토리지에 저장
     this.storage[key] = value;
@@ -45,8 +52,25 @@ const stringsStorage = new LocalStorage<string>();
 // get을 쓰면, string을 보내주고, T를 받음
 stringsStorage.get('ket');
 stringsStorage.set('hello', 'How are you');
+console.log(stringsStorage);
+// LocalStorage: {
+//   "storage": {
+//     "hello": "How are you"
+//   }
+// }
 
 const booleansStorage = new LocalStorage<boolean>();
 
 booleansStorage.get('xxx');
 booleansStorage.set('hello', true);
+console.log(booleansStorage);
+// LocalStorage: {
+//   "storage": {
+//     "hello": true
+//   }
+// }
+console.log(booleansStorage.get('hello')); // true
+console.log(booleansStorage);
+// LocalStorage: {
+//   "storage": {}
+// }
